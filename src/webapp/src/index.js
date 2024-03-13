b0987b6ad4b04dcf17d1210a6f138ae4801d62c5
thiagomsr20 <thiagoannotationdev2017@gmail.com>
2024-03-13 20:32:12 -0300

    Removidos arquivos da versao1

--- src/webapp/src/index.js
+++ src/webapp/src/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Cabecario from './component/Shared/cabecario/Cabecario'
-import Rodape from './component/Shared/rodape/Rodape'
-import BackgroundShape from './component/Shared/background-shape/background-shape'
-
-import Home from './component/Home/Home'
-
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Cabecario />
-    <Home />
-    <Rodape />
-    <BackgroundShape />
   </React.StrictMode>
 );
