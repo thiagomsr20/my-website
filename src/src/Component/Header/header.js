e9a53c2fe9942ae6bb1906d7eea75ccddb2f0060
thiagomsr20 <thiagoannotationdev2017@gmail.com>
2024-03-16 11:28:42 -0300

    att para lidar no PC

---
+++ src/src/Component/Header/header.js
@@ -0,0 +1,9 @@
+import './header.css'
+
+function Header(){
+    return (
+        <h1>header</h1>
+    );
+}
+
+export default Header
--- src/src/Index.js
+++ src/src/Index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+import Header from './Component/Header/header'
 import Footer from './Component/Footer/footer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
     <Footer />
   </React.StrictMode>
 );
