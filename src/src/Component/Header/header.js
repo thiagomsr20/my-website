ba0ca26375f7088a56b7574632ee66ed0835d4ec
thiagomsr20 <thiagoannotationdev2017@gmail.com>
2024-03-19 13:07:06 -0300

    ajustes iniciais do header

--- src/src/Component/Header/header.js
+++ src/src/Component/Header/header.js
@@ -1,8 +1,39 @@
 import './header.css'
 
-function Header(){
+function Header() {
     return (
-        <h1>header</h1>
+        <div className='header'>
+            <nav class="nav-bar">
+                <div class="logo">
+                    <h1>Logo</h1>
+                </div>
+                <div class="nav-list">
+                    <ul>
+                        <li class="nav-item"><a href="#" class="nav-link">Início</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Projetos</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link"> Sobre</a></li>
+                    </ul>
+                </div>
+                <div class="login-button">
+                    <button><a href="#">Entrar</a></button>
+                </div>
+
+                <div class="mobile-menu-icon">
+                    <button onclick="menuShow()"><i class="fas fa-bars"></i></button>
+                </div>
+            </nav>
+            <div class="mobile-menu">
+                <ul>
+                    <li class="nav-item"><a href="#" class="nav-link">Início</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Projetos</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Sobre</a></li>
+                </ul>
+
+                <div class="login-button">
+                    <button><a href="#">Entrar</a></button>
+                </div>
+            </div>
+        </div>
     );
 }
 
