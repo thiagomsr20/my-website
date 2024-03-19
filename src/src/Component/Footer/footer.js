4c4d6a63f87c725713502409c5a2500926a68647
thiagomsr20 <thiagoannotationdev2017@gmail.com>
2024-03-19 12:35:46 -0300

    Footer concluído

--- src/src/Component/Footer/footer.js
+++ src/src/Component/Footer/footer.js
@@ -4,41 +4,45 @@ function Footer() {
     return (
         <div className='footer'>
             <div className='footer-container'>
-                <div className='footer-container-message'>
+                <div className='container'>
+                    <div className='footer-subcontainer'>
 
-                    <h1>Thiago's Portfolio</h1>
-                    <p>Thank you for visiting my personal portfolio website.
-                        Connect with me over socials.
+                        <h1 className='footer-title'>Thiago's Portfolio</h1>
+                        <p className='footer-p1'>Thank you for visiting my personal portfolio website.<br></br>
+                            Connect with me over socials.
+                        </p>
+
+                    </div>
+                    <div className='footer-subcontainer'>
+
+                        <h1 className='footer-title'>Quick Links</h1>
+                        <ul className='footer-content'>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Skills</li>
+                            <li>Education</li>
+                            <li>Work</li>
+                            <li>Experience</li>
+                        </ul>
+
+                    </div>
+                    <div className='footer-subcontainer'>
+
+                        <h1 className='footer-title'>Contact Info</h1>
+                        <div className='footer-content'>
+                            <p>+55 54 99999-9999</p>
+                            <p>thiagoteste@gmail.com</p>
+                            <p>Brasil, RS</p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className='copyright-container'>
+                    <hr></hr>
+                    <p className='copyright'>
+                        Copyright &copy; 2024 Thiago Maciel. All Rights Reserved.
                     </p>
-
                 </div>
-                <div className='footer-container-quick_links'>
-
-                    <h1>Quick Links</h1>
-                    <ul>
-                        <p>Home</p>
-                        <p>About</p>
-                        <p>Skills</p>
-                        <p>Education</p>
-                        <p>Work</p>
-                        <p>Experience</p>
-                    </ul>
-
-                </div>
-                <div className='footer-container-contact_info'>
-
-                    <h1>Contact Info</h1>
-                    <p>+55 54 99999-9999</p>
-                    <p>thiagoteste@gmail.com</p>
-                    <p>Brasil, RS</p>
-
-                </div>
-            </div>
-            <div className='copyright-container'>
-                <hr></hr>
-                <p className='copyright'>
-                    Copyright © 2024 Thiago Maciel. All Rights Reserved.
-                </p>
             </div>
         </div>
     );
