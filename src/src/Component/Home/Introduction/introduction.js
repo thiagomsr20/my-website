13e784f8a0dde998db89145653e91ce9df31a3d7
thiagomsr20 <thiagoannotationdev2017@gmail.com>
2024-03-22 01:37:52 -0300

    Mensagem de site em construcao

---
+++ src/src/Component/Home/Introduction/introduction.js
@@ -0,0 +1,13 @@
+import './introduction.css'
+
+export default function Introduction() {
+    return (
+        <div className='Introduction'>
+            <div>
+                <h1>Cooming soon</h1>
+                <i class="fa-solid fa-gear"></i>
+            </div>
+            <img id="corgiGif" src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/58229245652299.583ac55194751.gif" alt="corgi gif"></img>
+        </div>
+    )
+}
--- src/src/Index.js
+++ src/src/Index.js
@@ -4,16 +4,18 @@ import './index.css';
 
 // Header e footer fixos na pagina
 import Header from './Component/Header/header'
+import Design_bgShape from './Component//Design/bg shape/bgShape'
 import Footer from './Component/Footer/footer'
 
 // Paginas variaveis
-import About from './Component/About/about'
+import Introduction from './Component/Home/Introduction/introduction'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
-    <About />
+    <Introduction />
+    <Design_bgShape totalPageHeight={1000}/>
     <Footer />
   </React.StrictMode>
 );
