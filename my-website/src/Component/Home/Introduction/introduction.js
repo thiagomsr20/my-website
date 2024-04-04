c4f6763958c2230eedc5e4947eb6cd1c581e553d
thiagomsr20 <thiagoannotationdev2017@gmail.com>
2024-04-04 17:05:12 -0300

    Atualizanfo responsividade e dark mode

--- my-website/src/Component/Home/Introduction/introduction.js
+++ my-website/src/Component/Home/Introduction/introduction.js
@@ -4,34 +4,32 @@ import { useGlobalState } from '../../../GlobalState'; // Importando o hook useG
 // Imagem de perfil
 import fotoPerfil from './resource/foto de perfil.png'
 
-const textLightMode = {
-    color: '#1d1d1d'
-}
-
-const textDarkMode = {
-    color: '#c4c4c4'
-}
+const textDarkMode = { color: '#c4c4c4' }, textLightMode = { color: '#1d1d1d' }
+const bgDark = { background: '#191919' }, bgLight = { background: '#fff' }
 
 export default function Introduction() {
     const { darkMode } = useGlobalState(); // Usando o hook useGlobalState para acessar o valor de darkMode
 
     return (
-        <div className='Introduction'>
-            <div className='foto'>
-                <img src={fotoPerfil} alt='foto perfil'></img>
-                <span></span>
-            </div>
+        <div className='Introduction' style={darkMode ? bgDark : bgLight}>
+            <div className='Introduction-container'>
 
-            <div className='apresentacao'>
-                <p>Hi there!</p>
-                <h1 style={darkMode ? textDarkMode : textLightMode}>
-                    I'm <span>Thiago</span>,<br></br>
-                    Software Developer
-                </h1>
-            </div>
+                <div className='apresentacao'>
+                    <p>Hi there!</p>
+                    <h1 style={darkMode ? textDarkMode : textLightMode}>
+                        I'm <span>Thiago</span>,<br></br>
+                        Software Developer
+                    </h1>
+                </div>
 
-            <div className='hireme'>
+                <div className='foto'>
+                    <img src={fotoPerfil} alt='foto perfil'></img>
+                    <span></span>
+                </div>
 
+                <div className='hireme'>
+
+                </div>
             </div>
         </div>
     )
