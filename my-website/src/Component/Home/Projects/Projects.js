974e68ea9585d2a10aba59f5525bd1f453afa21b
thiagomsr20 <thiagoannotationdev2017@gmail.com>
2024-04-08 00:52:23 -0300

    Seção de projetos finalizada

--- my-website/src/Component/Home/Projects/Projects.js
+++ my-website/src/Component/Home/Projects/Projects.js
@@ -5,7 +5,7 @@ import { useGlobalState } from '../../../GlobalState';
 
 const textDarkMode = { color: '#c4c4c4' }, textLightMode = { color: '#1d1d1d' }
 const bgDark = { background: '#232323' }, bgLight = { background: '#d9d9d9' }
-const subTextDark = { color: '#606060' }, subTextLight = { color: '#6f6f6f' }
+const boxShadowDark = { boxShadow: '0 5px 10px #000' }, boxShadowLight = { boxShadow: '0 5px 10px #5a5a5a' }
 
 export default function Projects() {
     const { darkMode } = useGlobalState();
@@ -15,7 +15,7 @@ export default function Projects() {
             <h1 style={darkMode ? textDarkMode : textLightMode}>My personal <span>projects</span></h1>
             <div className='project-cards'>
                 {projectData.map((project) => (
-                    <div className='project-card'>
+                    <div className='project-card' style={darkMode ? boxShadowDark : boxShadowLight}>
                         <img src={project.img} alt='project img'></img>
                     </div>
                 ))}
