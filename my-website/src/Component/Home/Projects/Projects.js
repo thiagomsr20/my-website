f6916ef93086e409a5f67d2588faa7d9cacc13ec
thiagomsr20 <thiagoannotationdev2017@gmail.com>
2024-04-16 22:15:59 -0300

    Cartoes com efeito 3d

--- my-website/src/Component/Home/Projects/Projects.js
+++ my-website/src/Component/Home/Projects/Projects.js
@@ -11,14 +11,36 @@ export default function Projects() {
     const { darkMode } = useGlobalState();
 
     return (
-        <div className='project'  style={darkMode ? bgDark : bgLight}>
+        <div className='project' style={darkMode ? bgDark : bgLight}>
             <h1 style={darkMode ? textDarkMode : textLightMode}>My personal <span>projects</span></h1>
             <div className='project-cards'>
                 {projectData.map((project) => (
-                    <div className='project-card' style={darkMode ? boxShadowDark : boxShadowLight}>
-                        <img src={project.img} alt='project img'></img>
+                    // <div className='project-card' style={darkMode ? boxShadowDark : boxShadowLight}>
+                    //     <img src={project.img} alt='project img'></img>
+                    // </div>
+
+                    <div class="parent">
+                        <div class="card">
+                            <div class="content-box">
+                                <span class="card-title">{project.title}</span>
+                                <p class="card-content">
+                                    {project.desc}
+                                </p>
+                                <span class="see-more">See More</span>
+                            </div>
+                            <div class="date-box">
+                                <span class="month">APRIL</span>
+                                <span class="date">16</span>
+                            </div>
+                        </div>
                     </div>
                 ))}
+
+
+
+
+
+
             </div>
         </div>
     )
