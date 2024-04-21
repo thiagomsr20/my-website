aea25e2457def59dc360094154d4e70f5b66c1ea
thiagomsr20 <thiagoannotationdev2017@gmail.com>
2024-04-21 13:23:21 -0300

    Seção de cartoes atualizada

--- my-website/src/Component/Home/Projects/Projects.js
+++ my-website/src/Component/Home/Projects/Projects.js
@@ -4,29 +4,25 @@ import projectData from './resources/projetosData.json'
 import { useGlobalState } from '../../../GlobalState';
 
 const textDarkMode = { color: '#c4c4c4' }, textLightMode = { color: '#1d1d1d' }
-const bgDark = { background: '#232323' }, bgLight = { background: '#e8e8e8' }
-const boxShadowDark = { boxShadow: '0 5px 10px #000' }, boxShadowLight = { boxShadow: '0 5px 10px #5a5a5a' }
+const bgDark = { background: '#232323' }, bgLight = { background: '#d4d4d4' }
 
 export default function Projects() {
     const { darkMode } = useGlobalState();
 
     return (
         <div className='project' style={darkMode ? bgDark : bgLight}>
-            <h1 style={darkMode ? textDarkMode : textLightMode}>My personal <span>projects</span></h1>
+            <h1 style={darkMode ? textDarkMode : textLightMode}>My personal<span>:projects:</span></h1>
             <div className='project-cards'>
                 {projectData.map((project) => (
-                    // <div className='project-card' style={darkMode ? boxShadowDark : boxShadowLight}>
-                    //     <img src={project.img} alt='project img'></img>
-                    // </div>
-
                     <div class="parent">
-                        <div class="card">
+                        <div class="card" 
+                        style={{ backgroundImage: `url(${project.img})` }}>
                             <div class="content-box">
                                 <span class="card-title">{project.title}</span>
                                 <p class="card-content">
                                     {project.desc}
                                 </p>
-                                <span class="see-more">See More</span>
+                                <a class="see-more" href={project.url} target='blank'>See More</a>
                             </div>
                             <div class="date-box">
                                 <span class="month">APRIL</span>
@@ -35,12 +31,6 @@ export default function Projects() {
                         </div>
                     </div>
                 ))}
-
-
-
-
-
-
             </div>
         </div>
     )
